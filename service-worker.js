a57c1969d25affed3666d46d6bb2089eadc4adc1
Terry9A9 <terryleung087@gmail.com>
2022-04-19 15:44:47 +0800

    pwa

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
 // List the files to precache
-const precacheResources = ['/', './index.html', './style.css', './app.js', './schoolData11.json'];
+const precacheResources = ['/313-project/', '/313-project/index.html', '/313-project/style.css', '/313-project/app.js', '/313-project/schoolData11.json'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
