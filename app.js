7dc312573e1abc0fe398744ffa08e1fcd6c73dcb
Terry9A9 <terryleung087@gmail.com>
2022-04-22 01:07:31 +0800

    pwa offline

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
+
+
+
 $(document).ready(function () {
 
-    var table = $('#originalTable').DataTable({
-        "ajax": "/313-project/schoolData11.json",
-        rowReorder: {
-            selector: 'td:nth-child(2)'
-        },
-        rowReorder: false,
-        responsive: true,
-        initComplete: function () {
+    function getData() {
+        const xhr = new XMLHttpRequest();
+        const url = "schoolData.json";
 
-            var filter_cols = [1, 2, 3, 4, 6, 7]
-            this.api().columns(filter_cols).every(function () {
-                var column = this;
-                var select = $('<select><option value=""></option></select>')
-                    .appendTo($(column.footer()).empty())
-                    .on('change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
+        return new Promise(function (resolve, reject) {
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    var schoolData = JSON.parse(xhr.response);
 
-                        column
-                            .search(val ? '^' + val + '$' : '', true, false)
-                            .draw();
-                    });
+                    // Store data data to localstorage
+                    const localStorage = window.localStorage;
+                    if (localStorage) {
+                        localStorage.setItem("schoolData", JSON.stringify(schoolData));
+                        console.log('[DATA][Promise] '+url+' stored in localStorage')
+                    }
+                    resolve();
+                }
+               
+            };
+            xhr.open("get", url);
+            xhr.send();
+        });
+    }
 
-                column.data().unique().sort().each(function (d, j) {
-                    select.append('<option value="' + d + '">' + d + '</option>')
-                });
-            });
-            filter_cols.forEach(col => {
-                this.api().columns(col).every(function () {
+    getData().then(function () {
+        console.log("[DATA] loading dataTable")
+        $('#originalTable').dataTable({
+            ajax: function (data, callback, settings) {
+                callback(
+                    JSON.parse(localStorage.getItem('schoolData'))
+                );
+            },
+            rowReorder: {
+                selector: 'td:nth-child(2)'
+            },
+            rowReorder: false,
+            responsive: true,
+            initComplete: function () {
+                var filter_cols = [1, 2, 3, 4, 6, 7]
+                this.api().columns(filter_cols).every(function () {
                     var column = this;
                     var select = $('<select><option value=""></option></select>')
-                        .appendTo($("#mobile-filter" + col))
+                        .appendTo($(column.footer()).empty())
                         .on('change', function () {
                             var val = $.fn.dataTable.util.escapeRegex(
                                 $(this).val()
@@ -47,39 +60,56 @@ $(document).ready(function () {
                         select.append('<option value="' + d + '">' + d + '</option>')
                     });
                 });
-            });
+                filter_cols.forEach(col => {
+                    this.api().columns(col).every(function () {
+                        var column = this;
+                        var select = $('<select><option value=""></option></select>')
+                            .appendTo($("#mobile-filter" + col))
+                            .on('change', function () {
+                                var val = $.fn.dataTable.util.escapeRegex(
+                                    $(this).val()
+                                );
 
-        },
-        "columns": [
-            {
-                data: "E", title: "名稱"
+                                column
+                                    .search(val ? '^' + val + '$' : '', true, false)
+                                    .draw();
+                            });
+
+                        column.data().unique().sort().each(function (d, j) {
+                            select.append('<option value="' + d + '">' + d + '</option>')
+                        });
+                    });
+                });
+
             },
-            { data: "U", title: "分區" },
-            { data: "W", title: "資助種類" },
-            { data: "Q", title: "就讀性別" },
-            { data: 'Y', title: "類型" },
-            { data: 'G', title: "地址" },
-            { data: 'S', title: "授課時間" },
-            { data: 'AG', title: "宗教" },
-            {
-                data: "AE", title: "URL",
-                render: function (data, type) {
-                    if (type === 'display') {
-                        let link = "Website";
-                        if (data.length <= 0) {
-                            return 'No Web Site';
+            "columns": [
+                { data: "E", title: "名稱" },
+                { data: "U", title: "分區" },
+                { data: "W", title: "資助種類" },
+                { data: "Q", title: "就讀性別" },
+                { data: 'Y', title: "類型" },
+                { data: 'G', title: "地址" },
+                { data: 'S', title: "授課時間" },
+                { data: 'AG', title: "宗教" },
+                {
+                    data: "AE", title: "URL",
+                    render: function (data, type) {
+                        if (type === 'display') {
+                            if (data.length <= 0) {
+                                return 'No Web Site';
+                            }
+                            else {
+                                return '<a href="' + data + '">' + "Website" + '</a>';
+                            }
                         }
-                        else {
-                            return '<a href="' + data + '">' + "Website" + '</a>';
-                        }
+                        return data;
                     }
-                    return data;
+
                 }
 
-            }
+            ],
 
-        ],
-
-    });
+        })
+    })
 });
 
--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,41 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
 // List the files to precache
-const precacheResources = ['/313-project/', '/313-project/index.html', '/313-project/style.css', '/313-project/app.js', '/313-project/schoolData11.json'];
+
+//local test
+//const preCacheResources = ['/', '/app.js', '/schoolData.json', '/service-worker.js','/manifest.json'];
+
+//deploy
+const preCacheResources = ['/313-project/', '/313-project/index.html', '/313-project/style.css', '/313-project/app.js', '/313-project/schoolData11.json'];
+
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
-  console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+  console.log('[Service worker] install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(preCacheResources))); 
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service worker activate event!');
+  console.log('[Service worker] activate event!');
+  self.clients.claim();
+  const clearCache = async () => {
+      const keys = await caches.keys();
+      keys.forEach(async (k) => {
+          if (cacheName.includes(k)) {
+              return;
+          }
+          await caches.delete(k);
+      });
+  };
+  event.waitUntil(clearCache());
 });
 
 // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
+  console.log('[Service worker] Fetch intercepted for:', event.request.url);
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
+    fetch(event.request).catch(function () {
+      return caches.match(event.request);
     }),
   );
 });
