fd50deed50682c36383595521dde7d1fe2dc84ce
Terry9A9 <terryleung087@gmail.com>
2022-04-23 17:56:07 +0800

    map done

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 let file_url;
 const file_name = "schoolData";
+
+
+function createMap(lat, long, name) {
+    // Creating map options
+    console.log(name)
+    let lat_long = [lat, long]
+    document.getElementsByClassName('modal-body')[0].innerHTML = "<div id='map'></div>";
+    document.getElementsByClassName('modal-title')[0].innerHTML = name
+    console.log(lat_long)
+    var mapOptions = {
+        center: lat_long,
+        zoom: 18
+    }
+    // Creating a map object
+    map = new L.map('map', mapOptions);
+    // Creating a Layer object
+    var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+    // Adding layer to the map
+    map.addLayer(layer);
+    // Creating a marker
+    var marker = L.marker(lat_long);
+    // Adding marker to the map
+    marker.addTo(map);
+    $('#modal-fullscreen-sm').on('shown.bs.modal', function () {
+        map.invalidateSize();
+    });
+}
+
 if (!navigator.onLine) {
     //if offline
     console.log("[Offline]")
@@ -24,14 +53,14 @@ if (!navigator.onLine) {
                         console.log('[Offline][Promise] ' + url + ' stored in localStorage')
                     }
                     console.log("[Offline] DataTable is using localStorage"),
-                    resolve();
+                        resolve();
                 }
             };
             xhr.open("get", url, true);
             xhr.send();
         });
     }
-}else{
+} else {
     //if online
     console.log("[Online]")
     file_url = "schoolData.json"
@@ -58,8 +87,8 @@ $(document).ready(function () {
                 "loadingRecords": "正在加載...",
                 "search": "搜尋",
                 "paginate": {
-                    "next":       "下一頁",
-                    "previous":   "上一頁"
+                    "next": "下一頁",
+                    "previous": "上一頁"
                 },
             },
             rowReorder: {
@@ -120,10 +149,15 @@ $(document).ready(function () {
                 { data: "U", title: "分區" },
                 { data: "W", title: "資助種類" },
                 { data: "Q", title: "就讀性別" },
-                { data: 'Y', title: "類型" },
-                { data: 'G', title: "地址" },
-                { data: 'S', title: "授課時間" },
-                { data: 'AG', title: "宗教" },
+                { data: "Y", title: "類型" },
+                {
+                    data: "G", title: "地址",
+                    render: function (data, type, row) {
+                        return data + `<button class="btn btn-primary btn-open-modal" data-toggle="modal" data-target="#modal-fullscreen-sm" id="mapButton" value="${row.E}" onclick=createMap(${row.K},${row.I},this.value)>Map</button>`
+                    }
+                },
+                { data: "S", title: "授課時間" },
+                { data: "AG", title: "宗教" },
                 {
                     data: "AE", title: "URL",
                     render: function (data, type) {
@@ -137,8 +171,13 @@ $(document).ready(function () {
                         }
                         return data;
                     }
-
-                }
+                },
+                { data: "K", title: "lat" },
+                { data: "I", title: "long" }
+            ],
+            "columnDefs": [
+                { "visible": false, "targets": [9] },
+                { "visible": false, "targets": [10] }
             ]
         })
     })
